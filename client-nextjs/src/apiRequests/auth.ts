4263ee1b983a40a561d81b197d0c25fe187d2ccb
MyChi <mychikhtn@gmail.com>
2026-01-14 19:46:13 +0700

    Xử lý logout với Next.js

--- client-nextjs/src/apiRequests/auth.ts
+++ client-nextjs/src/apiRequests/auth.ts
@@ -5,6 +5,7 @@ import {
   RegisterBodyType,
   RegisterResType,
 } from "@/src/schemaValidations/auth.schema";
+import { MessageResType } from "@/src/schemaValidations/common.schema";
 
 const authApiRequest = {
   login: (body: LoginBodyType) => http.post<LoginResType>("/auth/login", body),
@@ -12,6 +13,20 @@ const authApiRequest = {
     http.post<RegisterResType>("/auth/register", body),
   auth: (body: { sessionToken: string }) =>
     http.post("/api/auth", body, {
+      baseUrl: "",
+    }),
+  logoutFromNextServerToServer: (sessionToken: string) =>
+    http.post<MessageResType>(
+      "/auth/logout",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${sessionToken}`,
+        },
+      }
+    ),
+  logoutFromNextClientToNextServer: () =>
+    http.post("/api/auth/logout", null, {
       baseUrl: "",
     }),
 };
